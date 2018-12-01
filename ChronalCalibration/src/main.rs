ee1c939f59ecfd510adf1428a57a83d083dc975c
Andy Walsh <andyjasonwalsh@gmail.com>
2018-12-01 14:17:32 -0600

    MVP for parsing the token and drift value from the input file per line

--- ChronalCalibration/src/main.rs
+++ ChronalCalibration/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, Read};
+use std::io::{BufRead, BufReader};
 use std::fs::File;
 
 fn main() {
@@ -7,19 +7,29 @@ fn main() {
     let file = File::open("./data/input.txt").expect("Unable to open file.");
     let mut buf_read = BufReader::new(file);
 
-    buf_read.read_to_string(&mut payload).expect("Unable to parse.");
-    println!("{}", payload);
+    for line in buf_read.lines() {
+        let line = line.expect("Unable to read line.");
+        calculate_freq_drift(line);
+    }
+    // buf_read.read_to_string(&mut payload).expect("Unable to parse.");
+    // println!("{}", payload);
 }
 
 
-fn parse_token() {
+fn parse_token(l : &str) {
     
 }
 
-fn parse_line() {
+fn parse_line(l : &str) {
     
 }
 
-fn calculate_freq_drift() {
-    
+fn calculate_freq_drift(line : String) {
+    let mut freq = 0;
+    let len = line.len();
+    let slice_line: &str = &line[..];
+    let token = &slice_line[..1];
+    let drift = &slice_line[1..len];
+
+    println!("The token is {} and the value is {}", token, drift);
 }
