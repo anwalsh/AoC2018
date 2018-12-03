23c13cc152b8c1b39c5912fd0414bcc539bb2a21
Andy Walsh <andyjasonwalsh@gmail.com>
2018-12-02 23:05:01 -0600

    Function signature stubs, some logic to parse each line into a vec of Strings.

--- inventory_management/src/main.rs
+++ inventory_management/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./data/input.txt").expect("Unable to open file.");
+    let buf_read = BufReader::new(file);
+    let mut input: Vec<String> = Vec::new();
+    for line in buf_read.lines() {
+        let line: String = line.unwrap();
+        input.push(line.parse().unwrap());
+    }
+
+    generate_checksum(&input);
+}
+
+fn generate_checksum(input: &Vec<String>) {
+    for id in input {
+        println!("{}", id)
+    }
+}
+
+fn calculate_checksum(x: i64, y: i64) -> i64 {
+    x * y
 }
