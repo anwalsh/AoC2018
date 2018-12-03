3d8551720434856836432a4a9c40dc0642fcc6de
Andy Walsh <andyjasonwalsh@gmail.com>
2018-12-03 12:23:21 -0600

    Formatting changes, naming convention changes for clarity, and removed debug lines.

--- inventory_management/src/main.rs
+++ inventory_management/src/main.rs
@@ -15,39 +15,33 @@ fn main() {
 }
 
 fn generate_checksum(input: &Vec<String>) -> i64 {
-    let mut x = 0;
-    let mut y = 0;
+    let mut two = 0;
+    let mut three = 0;
 
     for id in input {
         let id_vec: Vec<char> = id.chars().collect();
-        let mut seen_twice = 0;
-        let mut seen_thrice = 0;
+        let mut _two = 0;
+        let mut _three = 0;
 
         for character in id_vec.iter() {
             let occurrences = id_vec.iter().filter(|c| c == &character).count();
 
-            if occurrences > 3 {
-                println!("{}", occurrences);
-            }
             match occurrences {
-                2 => seen_twice += 1,
-                3 => seen_thrice += 1,
+                2 => _two += 1,
+                3 => _three += 1,
                 _ => (),
             }
-            // println!("{}, {}", seen_twice, seen_thrice);
         }
-        if seen_twice >= 1 && seen_thrice >= 1 {
-            x += 1;
-            y += 1;
-        }
-        else if seen_twice >= 1 {
-            x += 1;
-        }
-        else if seen_thrice >= 1 {
-            y += 1;
+        if _two >= 1 && _three >= 1 {
+            two += 1;
+            three += 1;
+        } else if _two >= 1 {
+            two += 1;
+        } else if _three >= 1 {
+            three += 1;
         }
     }
-    calculate_checksum(x, y)
+    calculate_checksum(two, three)
 }
 
 fn calculate_checksum(x: i64, y: i64) -> i64 {
