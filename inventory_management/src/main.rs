9779620a8d4aad0e0bb95036513a240ab7a83b32
Andy Walsh <andyjasonwalsh@gmail.com>
2018-12-03 12:20:23 -0600

    Solution for Day 2 Part 1 implemented.

--- inventory_management/src/main.rs
+++ inventory_management/src/main.rs
@@ -10,13 +10,44 @@ fn main() {
         input.push(line.parse().unwrap());
     }
 
-    generate_checksum(&input);
+    let checksum = generate_checksum(&input);
+    println!("Checksum is: {}", checksum);
 }
 
-fn generate_checksum(input: &Vec<String>) {
+fn generate_checksum(input: &Vec<String>) -> i64 {
+    let mut x = 0;
+    let mut y = 0;
+
     for id in input {
-        println!("{}", id)
+        let id_vec: Vec<char> = id.chars().collect();
+        let mut seen_twice = 0;
+        let mut seen_thrice = 0;
+
+        for character in id_vec.iter() {
+            let occurrences = id_vec.iter().filter(|c| c == &character).count();
+
+            if occurrences > 3 {
+                println!("{}", occurrences);
+            }
+            match occurrences {
+                2 => seen_twice += 1,
+                3 => seen_thrice += 1,
+                _ => (),
+            }
+            // println!("{}, {}", seen_twice, seen_thrice);
+        }
+        if seen_twice >= 1 && seen_thrice >= 1 {
+            x += 1;
+            y += 1;
+        }
+        else if seen_twice >= 1 {
+            x += 1;
+        }
+        else if seen_thrice >= 1 {
+            y += 1;
+        }
     }
+    calculate_checksum(x, y)
 }
 
 fn calculate_checksum(x: i64, y: i64) -> i64 {
